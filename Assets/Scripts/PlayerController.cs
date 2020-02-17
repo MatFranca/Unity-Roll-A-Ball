c210248fb7548cd374edfd01a0cfd0e277f0acf1
MatFranca <math.francas@gmail.com>
2020-02-17 19:34:14 -0300

    Create Project
    Create repository and add the project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.gameObject.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0f, MoveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            Destroy(other.gameObject);
+            //other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 12)
+        {
+            //winText.text = "You Win!"
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
